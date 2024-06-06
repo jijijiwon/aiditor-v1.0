6fff180c67dae4cb992de8b3fc09ca591c5c3bef
jijijiwon <ljiwon0310@gmail.com>
2024-06-06 09:41:17 +0000

    fix

--- routes/moderation.js
+++ routes/moderation.js
@@ -4,12 +4,17 @@ var router = express.Router();
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 
+let NODE_SUB;
+
+router.use(function (req, res, next) {
+   NODE_SUB = "http://" + req.get("host").replace("8000", "8500");
+   next();
+ });
+
 const ID_M = process.env.ID;
 const SECRET_M = process.env.SECRET;
 const BUCKET_NAME_M = process.env.BUCKET_NAME;
 const MYREGION_M = process.env.REGION;
-const NODE_MAIN = process.env.NODE_MAIN;
-const NODE_SUB = process.env.NODE_SUB;
 const PYTHON_MAIN = process.env.PYTHON_MAIN;
 const PYTHON_SUB = process.env.PYTHON_SUB;
 
